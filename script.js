103c343add4daf0413d36bd708faab63e44520c3
emem-ette <elizabethudoessien03@gmail.com>
2022-06-23 10:02:25 -0700

    fm

---
+++ script.js
@@ -0,0 +1,5 @@
+let Name = "Elizabeth Udoessien";
+let height = "5'7"
+let country = "Nigeria";
+
+window.alert("Hey there, I'm " + Name + ", my height is " + height + " and I'm from " + country + ".");
